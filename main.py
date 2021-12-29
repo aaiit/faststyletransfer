b0fca038235b75acd35c7f1a5493993521f4658b
aaiit <abdellatif.aithammadi@gmail.com>
2021-12-29 12:26:40 +0100

    +

--- main.py
+++ main.py
@@ -12,15 +12,19 @@ import time
 import cv2
 
 
+# Load Transformer Network
+# net = transformer.TransformerNetwork()
+# net.load_state_dict(torch.load(f"./pretrained_models/{style}.pth", map_location=torch.device('cpu')))
+# net = net.to('cpu')
+
+pretrained_models = ['bayanihan','lazy', 'mosaic', 'starry', 'tokyo_ghoul', 'udnie', 'wave'] 
+
+nets = {style:transformer.TransformerNetwork() for style in pretrained_models}
+for style in pretrained_models:
+	nets[style].load_state_dict(torch.load(f"./pretrained_models/{style}.pth", map_location=torch.device('cpu')))
+	nets[style] = nets[style].to("cpu")
 
 def stylize(image, style, perserve_color = False, output_hight = None, output_width = None):
-
-	# Load Transformer Network
-	net = transformer.TransformerNetwork()
-	net.load_state_dict(torch.load(f"./pretrained_models/{style}.pth", map_location=torch.device('cpu')))
-	net = net.to('cpu')
-
-
 	with torch.no_grad():
 		torch.cuda.empty_cache()
 		print("Start styling image")
@@ -36,7 +40,7 @@ def stylize(image, style, perserve_color = False, output_hight = None, output_wi
 
 		starttime = time.time()
 		content_tensor = utils.itot(content_image).to('cpu')
-		generated_tensor = net(content_tensor)
+		generated_tensor = nets[style](content_tensor)
 		styled_image = utils.ttoi(generated_tensor.detach())
 		if perserve_color:
 			styled_image = utils.transfer_color(content_image, styled_image)
@@ -58,7 +62,6 @@ def main():
 	st.image(images, width=100, caption=indices)
 
 	# Get all the available models
-	pretrained_models = ['bayanihan','lazy', 'mosaic', 'starry', 'tokyo_ghoul', 'udnie', 'wave'] 
 	style = st.selectbox("select style:",pretrained_models)
 
 	uploaded_file = st.file_uploader("Choose a file")
